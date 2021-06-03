35acb2b95505d602f4a98734661721d6e4c2a0bb
Code Sandbox <hello@codesandbox.io>
2021-06-03 17:36:19 +0000

    Deployment from Codesandbox.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-document.write("Script.js");
+var phrase = "Y tu mama tambien";
+
+document.write(phrase.toUpperCase());
+
+//document.write(" " + Math.random()); gives a random decimal
+
+//gives a random whole number between 0 & 10
+document.write(" " + Math.round(Math.random() * 10));
+
+//this only gives random number between 1 & 0
+//document.write(" " + Math.round(Math.random()));
