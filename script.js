dc188bf5e53871cfd15cb0a49fec4fa4822c7d1c
Code Sandbox <hello@codesandbox.io>
2021-06-04 18:09:43 +0000

    Deployment from Codesandbox.

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-var phrase = "Y tu mama tambien";
-
-document.write(phrase.toUpperCase());
-
-//document.write(" " + Math.random()); gives a random decimal
-
-//gives a random whole number between 0 & 10
-document.write(" " + Math.round(Math.random() * 10));
-
-//this only gives random number between 1 & 0
-//document.write(" " + Math.round(Math.random()));
+var name = window.prompt("What's your name, Queen?");
+var age = window.prompt("How old are you?");
+document.write(
+  "Hey, " + name + "! How are ya? Do you like being " + age + " years old?"
+);
